c85d75e0330f02f610da6e5f707b8666dfa7c7f7
Stanimir Sergev <stanimir.sergev@hubparking.com>
2024-04-25 08:51:41 +0300

    add cross origin url.

--- src/main/java/com/example/productsdemo/Consts/Globals.java
+++ src/main/java/com/example/productsdemo/Consts/Globals.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Globals {
-    public static final String GLOBAL_FRONTEND_URL = "";
+    public static final String GLOBAL_FRONTEND_URL = "http://localhost:63342/";
     public static boolean IS_USER_LOGGED = false;
 }
